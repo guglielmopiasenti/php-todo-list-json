9559e6fcf31238ea3dc3614a43d688d1ff98ee61
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-20 16:54:23 +0200

    setting php to recieve data

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,6 +1,23 @@
 <?php
 // Initial data
-$tasks = ["HTML", "CSS", "Responsive Design", "JavaScript", "PHP"];
+$database_path = __DIR__ . '/../../database/tasks.json';
+
+// getting files from JSON
+$json_data = file_get_contents($database_path);
+
+// converting in PHP array 
+$tasks = json_decode($json_data, true);
+
+
+
+
+// checking POST
+$new_task = $_POST['task'] ?? null;
+if($new_task){
+    $tasks[] = $new_task;
+    $json_tasks = json_encode($tasks);
+    file_put_contents($database_path, $json_tasks);
+}
 
 // specifing that response is in JSON
 header('Content-type: application/json');

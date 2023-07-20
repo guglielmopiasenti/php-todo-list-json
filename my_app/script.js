021267415f1d08a97d796075770e1e93da22e036
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-20 16:56:22 +0200

    created + add task

--- my_app/script.js
+++ my_app/script.js
@@ -3,8 +3,25 @@ const { createApp } = Vue;
 const app = createApp({
     data(){
         return {
-            tasks:
+            tasks:[],
+            newTask:'',
         }
+    },
+    methods:{
+        addTask(){
+            // preparing data
+            const data = {'task': this.newTask};
+
+            // preparing confing
+            axios.post('http://localhost:8888/php-todo-list-json/api/tasks/', data, config)
+            .then()
+        }
+    },
+    created(){
+        axios.get('http://localhost:8888/php-todo-list-json/api/tasks/')
+        .then((res => {
+            this.tasks = res.data
+        }))
     }
 })
 

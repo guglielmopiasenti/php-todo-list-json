72a713acb39ec98f805590f0b79e45901b941734
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-22 18:48:41 +0200

    fixing json decode

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -4,9 +4,11 @@ require __DIR__ . '/../includes/opendb.php';
 
 // checking POST
 $task_text = $_POST['task'] ?? null;
+// converting in PHP array 
+$tasks = json_decode($json_data, true);
 if($task_text){
 
-    $new_task = ['text' => $task_text, 'done' => false, 'id' => count($tasks) + 1];
+    $new_task = ['id' => count($tasks) + 1, 'text' => $task_text, 'done' => false,];
 
     $tasks[] = $new_task;
     $json_tasks = json_encode($tasks);

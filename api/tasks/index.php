3dc6223e3361eb9fe6751fe8ad192be8d929a6e5
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-22 18:10:45 +0200

    modifying POST check

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -12,15 +12,19 @@ $tasks = json_decode($json_data, true);
 
 
 // checking POST
-$new_task = $_POST['task'] ?? null;
-if($new_task){
+$task_text = $_POST['task'] ?? null;
+if($task_text){
+
+    $new_task = ['text' => $task_text, 'done' => false, 'id' => count($tasks) + 1];
+
     $tasks[] = $new_task;
     $json_tasks = json_encode($tasks);
     file_put_contents($database_path, $json_tasks);
 }
 
-// specifing that response is in JSON
+// specifing that response is in JSON 
 header('Content-type: application/json');
+
 
 // Converting in JSON
 echo json_encode($tasks);

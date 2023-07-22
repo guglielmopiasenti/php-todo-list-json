e6620249bf8ebb6182fca93cee65ed750ed093af
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-22 19:35:51 +0200

    adding delete index.php

---
+++ api/tasks/delete/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../../includes/opendb.php';
+
+
+$id = $_POST['id'] ?? null;
+if($id){
+    // converting in PHP array 
+    $tasks = json_decode($json_data, true);
+    $updated_tasks = [];
+
+    foreach($tasks as $task){
+        if ($task['id'] != $id) $updated_tasks[] = $task;
+    }
+
+    $tasks = json_encode($updated_tasks);
+    file_put_contents($database_path, $json_tasks);
+}
+
+echo $tasks;
+
+?>

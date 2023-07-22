36698d0a55499d560b849f1b236884e7553a958a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-22 18:49:07 +0200

    removing json decode

--- api/includes/opendb.php
+++ api/includes/opendb.php
@@ -4,6 +4,3 @@ $database_path = __DIR__ . '/../../database/tasks.json';
 
 // getting files from JSON
 $json_data = file_get_contents($database_path);
-
-// converting in PHP array 
-$tasks = json_decode($json_data, true);
